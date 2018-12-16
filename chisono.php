465d26f451430f8ed0fbfad6ce1eaa99eb196b0c
Fury387 <webmaster@webtheory.it>
2018-12-17 00:51:37 +0100

    Fine Pagina chi sono

--- chisono.php
+++ chisono.php
@@ -20,8 +20,39 @@ Fidelizzazione e soddisfazione del cliente sono diventate da quel momento la fil
                 </div>
                 <!-- /.testo chi sono -->
             </div>
-        </div>
+        </div>    
+            <div class="row">
+	             <div class="col-lg-12 col-sm-12">
+	                <img src="/img/blocco%20foto%20netbi@2x.png" class="profilephoto"> 
+
+                </div>
+            </div>
+            
+            <div class="container">
+            <div class="row">
+	            
+	            <!-- testo netbi -->
+                <div class="col-lg-6 col-sm-12">
+	                <h2 class="rosso">L’UNIONE È LA NOSTRA FORZA</h2>
+	                <p>Tutto è nato nell’aprile del 2013, una scommessa sul futuro focalizzata sul mondo del web e dell’informatica. <br>Il mio obiettivo era chiaro ma non per questo di semplice realizzazione: implementare un gruppo di lavoro in grado di fornire ai propri clienti molteplici servizi e benefici. <br>Erogare un servizio a 360°, dalla creazione di siti web alla fornitura di apparecchiature informatiche, dalla consulenza specializzata alla riparazione di smartphone e device di vario tipo in un ambito territoriale circoscritto (per ora!) alla provincia di Biella. <br>
+Fidelizzazione e soddisfazione del cliente sono diventate da quel momento la filosofia aziendale di Web Theory. <br>Le collaborazioni sono la colonna portante di questo progetto che, non a caso, ha individuato nel <strong class="blu">SellaLab</strong> un perfetto incubatore da cui trarre spinta ed energia, dal quale da febbraio 2015 ho traggo nuova linfa vitale nuove collaborazioni, idee ed esperienze da vivere con entusiasmo e passione. </p>
+
+                </div>
+                <!-- /.testo netbi -->
+
+	            
+                <div class="col-lg-6 col-sm-12">
+	                <img src="/img/logo%20netbi@2x.png"> 
+
+                </div>
+                <!-- /.immagine foto chi sono -->
+               
+            </div>
+        </div>    
+
+        
     
   <?php /* ########################################## INCLUDE - INIZIO ################################################## */
+include($_SERVER['DOCUMENT_ROOT']."/includes/structures/calltoaction.php"); 
 include($_SERVER['DOCUMENT_ROOT']."/includes/structures/footer.php");
 /* ################################################ INCLUDE - FINE ################################################# */ ?>
