d21ef2314e5095339cb4b8a7104437076d0cb932
Fury387 <webmaster@webtheory.it>
2019-02-25 21:57:08 +0100

    Ottimizzazione responsive pagina consulenza

--- assistenza.php
+++ assistenza.php
@@ -23,30 +23,30 @@ include($_SERVER['DOCUMENT_ROOT']."/includes/structures/nav.php");
         </div>
 		<div class="col-lg-6 col-xs-12">
 			<div class="nav-item col-lg-4 col-md-6">
-				<img style="width: 50%;" src="img/riparazione-01.svg">
+				<img class="icon" src="img/riparazione-01.svg">
 				<h3>Riparazioni
 				</h3>
 			</div>
 			<div class="nav-item col-lg-4 col-md-6">
-				<img style="width: 50%;" src="img/retidati-02.svg">
+				<img class="icon" src="img/retidati-02.svg">
 				<h3>Rete dati</h3>						
 			</div>
 			<div class="nav-item col-lg-4 col-md-6">
-				<img style="width: 50%;" src="img/remota.svg">
+				<img class="icon" src="img/remota.svg">
 				<h3>Assistenza remota</h3>						
 			</div>
 		</div>
 		<div class="col-lg-6 col-xs-12">
 			<div class="nav-item col-lg-4 col-md-6">
-				<img style="width: 50%;" src="img/configurazione-04.svg">
+				<img class="icon" src="img/configurazione-04.svg">
 				<h3>Configurazioni</h3>
 			</div>
 			<div class="nav-item col-lg-4 col-md-6">
-				<img style="width: 50%;" src="img/assemblaggio-05.svg">
+				<img class="icon" src="img/assemblaggio-05.svg">
 				<h3>Assemblaggio PC</h3>
 			</div>
 			<div class="nav-item col-lg-4 col-md-6">
-				<img style="width: 50%;" src="img/allestimenti-06.svg">
+				<img class="icon" src="img/allestimenti-06.svg">
 				<h3>Allestimenti uffici</h3>	
 			</div>
 		</div>
@@ -76,15 +76,15 @@ SCARICA SUBITO TEAMVIEWER QUICK SUPPORT. </h3>
             <div class="row">
 	            <h2>Le manutenzioni che mi chiedete più spesso</h2>
 				<div class="row rowpadding">
-                	<div class="col-lg-12 col-sm-12">
-						<div class="nav-item col-lg-4 col-sm-4">
-							<img src="img/sitiweb%20icon.svg"><h3>"Il mio pc è lentissimo. Mi aiuti?"</h3>
+                	<div class="col-lg-12">
+						<div class="navitem col-lg-4 col-md-12">
+							<img class="icon" src="img/ssd-storage.svg"><h3>"Il mio pc è lentissimo. Mi aiuti?"</h3>
 								<p class="textcenter">Lo sai che almeno 3 volte a settimana mi viene fatta questa domanda? <br>
 Il mio consiglio non è quasi mai di sostituire il computer con una spesa importante, bensì di lavorare sulle ottimizzazioni di quello già in tuo possesso.<br>
 Lo sai che spesso risolvo il problema con un disco a stato solido (SSD)? Così il tuo computer torna a volare. </p>
 						</div>
-						<div class="nav-item col-lg-4 col-sm-4">
-							<img src="img/repair%20icon.svg"><h3>"Temo di aver preso un virus.."</h3>
+						<div class="col-lg-4 col-md-12">
+							<img class="icon" src="img/computer-virus.svg"><h3>"Temo di aver preso un virus.."</h3>
 								<p class="textcenter">È la paura di molti di voi che vi rivolgete a me. <br>
 Lo so, non è piacevole e ti dico una grande verità: può succedere e si può prevenire.<br>
 Ti garantisco che nel 85% dei casi posso risolvere il problema e insegnarti come non far ammalare più il tuo computer.<br>
@@ -92,10 +92,10 @@ Come? In 2 modi:<br>
 - Con un buon antivirus, un sistema di Backup, un'eventuale Nas.<br>
 - Se preferisci non pensarci tu, la soluzione "Pacchetti di ticket" fa per te! </p>
 						</div>
-						<div class="nav-item col-lg-4 col-sm-4">
-							<img src="img/consulenza%20icon.svg"><h3>A casa e in ufficio internet va malissimo. Tu puoi fare qualcosa?</h3>
+						<div class="col-lg-4 col-md-12">
+							<img class="icon" src="img/wifi.svg"><h3>A casa e in ufficio internet va malissimo. Tu puoi fare qualcosa?</h3>
 								<p class="textcenter">Sai che il 68,5% delle persone di 6 anni e più si è connesso alla Rete negli ultimi 12 mesi (65,3% nel 2017) mentre il 52,1% accede tutti i giorni? <br>
-(fonte Istat https://www.istat.it/it/archivio/226240)
+(fonte Istat: <a href="https://www.istat.it/it/archivio/226240">Link</a>)
 Io posso aiutarti a rimanere in questa maggioranza.<br>
 Se anche internet via cavo a casa tua o nelle tua azienda non prende, lo sai che possiamo valutare altre soluzioni economiche via etere?<br>
 Da domani il tuo internet inizierà ad andare benissimo. </p>
