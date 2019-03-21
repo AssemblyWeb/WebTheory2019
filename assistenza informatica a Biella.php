1499ab7c2673cc024687a8e93c0ba63e2a39a8cb
Fury387 <webmaster@webtheory.it>
2019-03-21 18:44:57 +0100

    Ottimizzazione SEO

---
+++ assistenza informatica a Biella.php
@@ -0,0 +1,162 @@
+<?php /* ########################################## INCLUDE - INIZIO ################################################## */
+include($_SERVER['DOCUMENT_ROOT']."/includes/structures/header.php");
+include($_SERVER['DOCUMENT_ROOT']."/includes/structures/nav.php"); 
+/* ################################################ INCLUDE - FINE ################################################# */ ?>
+
+
+<div class="container">
+	<div class="row">
+        <div class="col-lg-6">  
+	        <h2>La soluzione ai tuoi problemi</h2>  
+			<p> Contattami per:
+				<ul class="testo">
+					<li><p class="testo">Riparazioni di smartphone, Pc, Mac</p></li>
+					<li><p class="testo">Configurazioni (es. posta elettronica)</p></li>
+					<li><p class="testo">Connettività</p> </li>
+					<li><p class="testo">Reti di dati</p> </li>
+					<li><p class="testo">Sicurezza</p> </li>
+					<li><p class="testo">Assemblaggi di pc</p></li>
+					<li><p class="testo">Assistenza remota</p> </li>
+					<li><p class="testo">Allestimenti studi e uffici</p></li>
+					
+				</ul>
+				<p>Lo sai che ti posso aiutare anche da remoto?
+Continua a leggere e scopri come fare per usare TeamViewer insieme."</p>
+</p>
+        </div>
+		<div class="col-lg-6 col-xs-12">
+			<div class="nav-item col-lg-4 col-md-6">
+				<img class="icon" src="img/riparazione-01.svg">
+				<h3>Riparazioni
+				</h3>
+			</div>
+			<div class="nav-item col-lg-4 col-md-6">
+				<img class="icon" src="img/retidati-02.svg">
+				<h3>Rete dati</h3>						
+			</div>
+			<div class="nav-item col-lg-4 col-md-6">
+				<img class="icon" src="img/remota.svg">
+				<h3>Assistenza remota</h3>						
+			</div>
+		</div>
+		<div class="col-lg-6 col-xs-12">
+			<div class="nav-item col-lg-4 col-md-6">
+				<img class="icon" src="img/configurazione-04.svg">
+				<h3>Configurazioni</h3>
+			</div>
+			<div class="nav-item col-lg-4 col-md-6">
+				<img class="icon" src="img/assemblaggio-05.svg">
+				<h3>Assemblaggio PC</h3>
+			</div>
+			<div class="nav-item col-lg-4 col-md-6">
+				<img class="icon" src="img/allestimenti-06.svg">
+				<h3>Allestimenti uffici</h3>	
+			</div>
+		</div>
+	</div>
+</div>
+<!-- /.container -->
+  
+ <!-- section-d -->   
+<div class="content-section-d">
+	<div class="container">
+    	<div class="row rowpadding">
+        	<div class="col-lg-12 col-sm-12">
+	        	<img class="imgcenter" src="/img/TeamV.svg">
+	            	<h3 class="textcenter" style="color: white;">HAI BISOGNO DI ASSISTENZA SUL TUO COMPUTER?<br>
+SCARICA SUBITO TEAMVIEWER QUICK SUPPORT. </h3>
+						<a href="sitiweb.php"><div class="btn btn-wt imgcenter">Download</div></a>
+            </div>
+        </div>
+	</div>
+		
+
+<!-- content-section-b -->
+    <div class="content-section-b">
+
+        <div class="container">
+				
+            <div class="row">
+	            <h2>Le manutenzioni che mi chiedete più spesso:</h2>
+				<div class="row rowpadding">
+                	<div class="col-lg-12">
+						<div class="col-lg-10 col-md-12">
+							<h3>"Il mio pc è lentissimo. Mi aiuti?"</h3>
+								<p>Lo sai che almeno 3 volte a settimana mi viene fatta questa domanda? <br>
+Il mio consiglio non è quasi mai di sostituire il computer con una spesa importante, bensì di lavorare sulle ottimizzazioni di quello già in tuo possesso.<br>
+Lo sai che spesso risolvo il problema con un disco a stato solido (SSD)? Così il tuo computer torna a volare. </p>
+						</div>
+						<div class="col-lg-2">
+							<img class="icon" src="img/ssd-storage.svg">
+						</div>
+                	</div>
+                	
+                	<div class="col-lg-12">
+	                	<div class="col-lg-2">
+							<img class="icon" src="img/computer-virus.svg">
+						</div>
+						
+						<div class="col-lg-10 col-md-12">
+							<h3>"Temo di aver preso un virus.."</h3>
+								<p>È la paura di molti di voi che vi rivolgete a me. <br>
+Lo so, non è piacevole e ti dico una grande verità: può succedere e si può prevenire.<br>
+Ti garantisco che nel 85% dei casi posso risolvere il problema e insegnarti come non far ammalare più il tuo computer.<br>
+Come?<br>
+Con un buon antivirus, un sistema di Backup o un'eventuale Nas.<br>
+</p> 
+						</div>
+                	</div>
+                	<div class="col-lg-12">
+						<div class="col-lg-10 col-md-12">
+							<h3>A casa e in ufficio internet va malissimo. Tu puoi fare qualcosa?</h3>
+								<p>Sai che il 68,5% delle persone di 6 anni e più si è connesso alla Rete negli ultimi 12 mesi (65,3% nel 2017) mentre il 52,1% accede tutti i giorni? 
+(fonte Istat: <a href="https://www.istat.it/it/archivio/226240">Link</a>)<br>
+Io posso aiutarti a rimanere in questa maggioranza.<br>
+Se anche internet via cavo a casa tua o nelle tua azienda non prende, lo sai che possiamo valutare altre soluzioni economiche via Parabola?<br>
+Con il wireless la tua connettività non avrà più confini.<br>
+Il tuo portafoglio ringrazierà.<br>
+I tuoi figli e i tuoi collaboratori andranno più veloci e si lamenteranno meno. </p>
+						</div>	
+						<div class="col-lg-2">
+							<img class="icon" src="img/wifi.svg">
+						</div>
+					</div>
+                </div>
+            </div>
+            <div class="row">
+	            <div class="col-lg-3">
+	            </div>
+	            <div class="col-lg-6">
+		            <h3 class="textcenter">Se preferisci non pensarci tu, la soluzione "Pacchetti di ticket" fa per te!</h3>
+				<a href="/consulenza%20informatica.php"><div class="btn btn-wt imgcenter">Scopri</div></a>
+
+	            </div>
+	            <div class="col-lg-3">
+	            </div>
+            </div>
+				        </div>
+        <!-- /.container -->
+
+   	</div>
+   	<!-- /.content-section-b -->
+</div>
+<!-- /.content-section-d -->
+	<!-- content-section-f 
+<div class="content-section-f">
+	<div class="container">
+		<div class="row">
+	    	<h2>I marchi che tratto</h2>
+		</div>
+		<div class="row rowpadding">
+        	<div class="col-lg-12 col-sm-12">
+	        		
+        	</div>
+        </div>
+	</div>
+</div> -->
+<!-- /.content-section-f -->
+    
+  <?php /* ########################################## INCLUDE - INIZIO ################################################## */
+include($_SERVER['DOCUMENT_ROOT']."/includes/structures/calltoaction.php"); 
+include($_SERVER['DOCUMENT_ROOT']."/includes/structures/footer.php");
+/* ################################################ INCLUDE - FINE ################################################# */ ?>
